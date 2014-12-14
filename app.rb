83755f3c13bb060976e503ccd5b5ac062e1ee14b
Min McKnight <min.mcknight@gmail.com>
2014-12-14 12:01:37 -0500

    added login and logout Closes #2

--- app.rb
+++ app.rb
@@ -9,22 +9,60 @@ require_relative './models/events_user'
 require_relative './models/events_users_feature'
 require_relative './config/environments'
 
+enable :sessions
+
 helpers do
   def current_user
-    @current_user || nil
+     User.find(session[:user_id])
   end
 
   def current_user?
-    @current_user == nil ? false : true
+    !session[:user_id].nil?
   end
 end
-
+before do
+  @errors ||= []
+end
 get '/' do
   erb :index
 end
 
 post '/signup' do
-  puts "here?"
   @user =User.create!(name: params[:name],email: params[:email], password: params[:password],password_confirmation: params[:password_confirmation])
   return "signed up"
 end
+
+post '/login' do
+  @user = User.find_by(email: params[:email])
+  if @user
+    if @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect("/user")  
+    else
+      @errors << "incorrect password or email"
+      erb :index
+    end
+  else
+    @errors << "invalid email"
+    erb :index
+  end
+end
+
+get '/user' do
+  @user=current_user
+  erb :user_home
+end
+get "/logout" do
+  session.clear
+  redirect('/')
+end
+
+
+
+
+
+
+
+
+
+
