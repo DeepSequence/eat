904eb7861f1337de912ad43dc61f2725577f57f0
Min McKnight <min.mcknight@gmail.com>
2014-12-14 11:18:34 -0500

    allow user to sign up fixes #1

--- app.rb
+++ app.rb
@@ -9,7 +9,22 @@ require_relative './models/events_user'
 require_relative './models/events_users_feature'
 require_relative './config/environments'
 
+helpers do
+  def current_user
+    @current_user || nil
+  end
+
+  def current_user?
+    @current_user == nil ? false : true
+  end
+end
+
 get '/' do
-  @users = User.all
   erb :index
+end
+
+post '/signup' do
+  puts "here?"
+  @user =User.create!(name: params[:name],email: params[:email], password: params[:password],password_confirmation: params[:password_confirmation])
+  return "signed up"
 end
