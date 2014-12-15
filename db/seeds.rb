e4494cc6d37c7925b9277fd9e3fdfc05c040b884
Min McKnight <min.mcknight@gmail.com>
2014-12-15 10:43:53 -0500

    created restaurant preferences seeds file

---
+++ db/seeds.rb
@@ -0,0 +1,32 @@
+require_relative './features_restaurants'
+
+#this should somehouse be turned into a pop up window with a checkmark radio buttons
+features_restaurants.create(
+  [
+    { name: "Sandwiches"},
+    { name: "Pizza"},
+    { name: "American(Traditional)"},
+    { name: "Chinese"},
+    { name: "Burgers"},
+    { name: "Sandwiches"},
+    { name: "Delis"},
+    { name: "Mexican"},
+    { name: "Italian"},
+    { name: "Seafood"},
+    { name: "Japanese"},
+    { name: "Salad"},
+    { name: "Sushi"},
+    { name: "Thai"},
+    { name: "Indian"},
+    { name: "Mediterranean"},
+    { name: "Korean"},
+    { name: "Asian Fusion"},
+    { name: "soup"},
+    { name: "vegetarian"},
+    { name: "vegan"},
+    { name: "ramen"},
+    { name: "good coffee"},
+    { name: "good dessert"},
+
+  ]
+)
--- models/event.rb
+++ models/event.rb
@@ -1,4 +1,6 @@
 class Event < ActiveRecord::Base
+ has_many :events_users
  has_many :users, through: :events_users
  belongs_to :restaurant
+
 end
